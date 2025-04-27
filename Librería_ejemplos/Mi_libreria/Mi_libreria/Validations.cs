5a21ee8fdf4c07f8aa7cf4c4f0ad47c51fb4bf42
Samuel <olmos.samu@gmail.com>
2025-04-27 10:23:53 -0300

    Agregando librería con validación de entradas y menú de selección

---
+++ Librería_ejemplos/Mi_libreria/Mi_libreria/Validations.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mi_libreria
+{
+    public class Validations
+    {
+        public static int Numbers_only_input()
+        {
+            bool fin = false; //Variable para Do While
+            string valor = ""; //Cadena que se almacena para return
+
+            //"Validando" que sea un número
+            do
+            {
+                ConsoleKeyInfo k = Console.ReadKey(true); 
+                //Lo que se presione no se muestra en pantalla con el True
+                //Console.WriteLine((int)k.KeyChar);
+
+                //Finalizamos el ciclo While
+                if (k.Key == ConsoleKey.Enter)
+                {
+                    if (valor != "")
+                    {
+                        fin = true;
+                    }
+                }
+
+                //Borrando caracteres con Backspace
+                else if (k.Key == ConsoleKey.Backspace)
+                {
+                    if (valor.Length > 0) //Que no borre más allá de lo que se toma por entrada
+                    {
+                        //Borrando el último número de la consola
+                        Console.Write("\b");
+                        Console.Write(" ");
+                        Console.Write("\b");
+
+                        //Borrando el último número de la cadena almacenada 
+                        valor = valor.Substring(0, valor.Length - 1); 
+                    }
+                }
+
+                //Mostrando por pantalla los valores que se están presionando (solo números)
+                if ((int)k.KeyChar >= 48 && (int)k.KeyChar <= 57)
+                {
+                    Console.Write(k.KeyChar); //Por pantalla
+                    valor = valor + k.KeyChar; //En la cadena
+                }
+            } while (!fin);
+            Console.WriteLine();
+            return int.Parse(valor); //Retorna un int
+        }
+
+        public static string Letters_only_input()
+        {
+            bool fin = false; //Variable para Do While
+            string valor = ""; //Cadena que se almacena para return
+
+            //"Validando" que sea una letra
+            do
+            {
+                ConsoleKeyInfo k = Console.ReadKey(true);
+                //Lo que se presione no se muestra en pantalla con el True
+                //Console.WriteLine((int)k.KeyChar);
+
+                if (k.Key == ConsoleKey.Enter)
+                {
+                    if (valor != "")
+                    {
+                        fin = true;
+                    }
+                    
+                }
+                //Borrando caracteres con Backspace
+                else if (k.Key == ConsoleKey.Backspace)
+                {
+                    if (valor.Length > 0) //Que no borre más allá de lo que se toma por entrada
+                    {
+                        //Borrando la última letra de la consola
+                        Console.Write("\b");
+                        Console.Write(" ");
+                        Console.Write("\b");
+
+                        //Borrando la última letra de la cadena almacenada 
+                        valor = valor.Substring(0, valor.Length - 1);
+                    }
+                }
+
+                else if (k.Key == ConsoleKey.Spacebar)
+                {
+                    Console.Write(" ");
+                    valor = valor + " ";
+                }
+
+                //Mostrando por pantalla los valores que se están presionando (solo letras)
+                if (((int)k.KeyChar >= 65 && (int)k.KeyChar <= 90) || ((int)k.KeyChar >= 97 && (int)k.KeyChar <= 122) || (int)k.KeyChar == 225 || (int)k.KeyChar == 233 || (int)k.KeyChar == 237
+                    || (int)k.KeyChar == 243 || (int)k.KeyChar == 250 || (int)k.KeyChar == 241 || (int)k.KeyChar == 209)
+                {
+                    Console.Write(k.KeyChar); //Por pantalla
+                    valor = valor + k.KeyChar; //En la cadena
+                }
+            } while (!fin);
+            Console.WriteLine();
+            return valor; //Retorna un string
+        }
+
+
+    }
+
+    public class Selection_Menu
+    {
+        public static int Print(string title, int index, string[] options)
+        {
+            bool flag = true;
+            string max_lenght_word = "";
+            foreach (string word in options.Append(title))
+            {
+                if (max_lenght_word.Length < word.Length) max_lenght_word = word;
+            }
+            do
+            {
+                Console.Clear();
+
+                Console.Write("╔");
+                for (int i = 0; i < max_lenght_word.Length+2; i++) Console.Write("═");
+                Console.WriteLine("╗");
+
+                Console.Write("║");
+                for (int i = 0; i < (max_lenght_word.Length+2-title.Length)/2; i++) Console.Write(" ");
+                Console.Write(title);
+                for (int i = 0; i < (max_lenght_word.Length+2-title.Length)/2; i++) Console.Write(" ");
+                Console.WriteLine("║");
+
+                Console.Write("╠");
+                for (int i = 0; i < max_lenght_word.Length+2; i++) Console.Write("═");
+                Console.WriteLine("╣");
+
+
+                foreach (string word in options)
+                {
+                    Console.Write("║");
+                    if (word == options[index]) Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.Write(word);
+                    Console.ResetColor();
+                    for (int i = 0; i < max_lenght_word.Length+2-word.Length; i++) Console.Write(" ");
+                    Console.WriteLine("║");
+                }
+                Console.Write("╚");
+                for (int i = 0; i < max_lenght_word.Length+2; i++) Console.Write("═");
+                Console.WriteLine("╝");
+
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.UpArrow && index >= options.Length-(options.Length-1)) index--;
+
+                if (key.Key == ConsoleKey.DownArrow && index < options.Length-1) index++;
+
+                if (key.Key == ConsoleKey.Enter) flag = false;
+            } while (flag);
+            return index;
+        }
+    }
+}
---
+++ Librería_ejemplos/Probando_Mi_libreria/Probando_Mi_libreria/Program.cs
@@ -0,0 +1,20 @@
+﻿using Mi_libreria;
+namespace Probando_Mi_libreria
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            //int num = Validations.Numbers_only_input();
+            //string str = Validations.Letters_only_input();
+            string[] options = { "Árbol", "Casa", "Auto", "Carpa", "Manzana", "Mano" };
+
+            //Console.WriteLine(num);
+            //Console.WriteLine(str);
+            
+            Console.WriteLine(options[Selection_Menu.Print("Opciones", 2, options)]);
+
+        }
+    }
+}
